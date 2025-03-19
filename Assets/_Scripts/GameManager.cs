b51506643a8cb64ba286c6083dd86e49f1010ea9
TakersJP <97802658+TakersJP@users.noreply.github.com>
2025-03-18 18:57:06 -0700

    Added the number of lives

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,16 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
-    [SerializeField] private int maxLives = 3;
+    [SerializeField] private int startingLives = 3; // starting number of lives
+    private int maxLives; // track current lives
+
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
-
     [SerializeField] private ScoreCounterUI scoreCounter;
-    private int score = 0;
+
+    [SerializeField] private List<GameObject> heartObjects; // list of heart GameObjects (instead of just Images)
 
     private int currentBrickCount;
     private int totalBrickCount;
+
+    private int score = 0;
 
     private void OnEnable()
     {
@@ -18,6 +25,8 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         ball.ResetBall();
         totalBrickCount = bricksContainer.childCount;
         currentBrickCount = bricksContainer.childCount;
+        maxLives = startingLives; // initialize current lives
+        UpdateHeartsUI(); // update heart icons when game starts
     }
 
     private void OnDisable()
@@ -38,20 +47,36 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         currentBrickCount--;
         AddScore(1);
         Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
-        if(currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
+        if (currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
     }
 
     public void KillBall()
     {
         maxLives--;
         // update lives on HUD here
-        // game over UI if maxLives < 0, then exit to main menu after delay
-        ball.ResetBall();
+        UpdateHeartsUI();
+        // game over UI if maxLives < 1, then exit to main menu after delay
+        if (maxLives <= 0)
+        {
+            SceneHandler.Instance.LoadMenuScene();
+        }
+        else
+        {
+            ball.ResetBall();
+        }
     }
-    
+
     public void AddScore(int amount)
     {
         score += amount;
         scoreCounter.UpdateScore(score);
+    }
+
+    private void UpdateHeartsUI()
+    {
+        for (int i = 0; i < heartObjects.Count; i++)
+        {
+            heartObjects[i].SetActive(i < maxLives);
+        }
     }
 }
